1c611d3f60eae6254e087b11d828bdfd2f0e5be9
Nathan Crum <ncrum@mbp-ncrum.local>
2019-05-01 10:19:16 -0400

    clean data now runs and works finally, starting on anlysis.py

---
+++ analysis.py
@@ -0,0 +1,23 @@
+
+
+#TODO
+# calculate all of the fields that rick wants
+# put them in excel
+
+rf = .0024 # assumptions of the market
+mrp = .0062
+newcolumns = ['Equity COC', 'PVEPS', 'EPS1', 'EPS2', 'EPS3', 'EPS4', 'EPS5', 'Known Growth', 'Unknown Growth',
+              '% Present', '% Known', '% Unknown']
+
+
+def main():
+
+
+    # read in the clean data csv
+    df = pd.read_csv('sp1500clean.csv')
+
+
+
+
+if True:
+    main()
--- cleandata.py
+++ cleandata.py
@@ -1,8 +1,5 @@
 
 import pandas as pd
-from io import StringIO
-from datetime import datetime
-from csv import writer
 import re
 
 #TODO:
@@ -11,13 +8,18 @@ import re
 
 def turnmillionsandbillions(y):
 
-    x = re.split('(M|B)', y)
+    if y == '-':
+        return y
+    if y == str(0):
+        return y
+    #print(y)
+    x = re.split('(M|B)', str(y))
     num = float(x[0])
     letter = x[1]
 
     if letter == 'M':
         num *= 1000000
-    elif letter == 'B':
+    else:
         num *= 1000000000
 
     return num
@@ -25,8 +27,32 @@ def turnmillionsandbillions(y):
 
 def cleanpercent(z):
 
+    if z == '-':
+        return z
+    print(z)
     per = re.split('%', z)
-    return float(per[0])/100
+    print(per[0])
+    if per[0][0].isdigit() or per[0][0] == '-':
+        return float(per[0]) / 100
+    return '-'
+
+
+
+def emptytodash(a):
+
+    if a == 'NaN':
+        a = '-'
+    elif a == 'nan':
+        a = '-'
+    elif a == '':
+        a = '-'
+    elif a != a:
+        a = '-'
+    elif a is None:
+        return '-'
+    elif a == '\N{infinity}':
+        a = '-'
+    return a
 
 
 def main():
@@ -36,6 +62,8 @@ def main():
     #df[] is a series and uses apply
     #df[[]] is a dataframe and uses applymap
 
+    df = df.applymap(lambda x: emptytodash(x))
+
     # clean up millions and billions
     df['Shares Outstanding'] = df['Shares Outstanding'].apply(lambda x: turnmillionsandbillions(x))
     df['Market Cap'] = df['Market Cap'].apply(lambda x: turnmillionsandbillions(x))
@@ -44,7 +72,7 @@ def main():
     df['Expected Growth - 5 Year'] = df['Expected Growth - 5 Year'].apply(lambda x: cleanpercent(x))
     df['Last 12 Month Return'] = df['Last 12 Month Return'].apply(lambda x: cleanpercent(x))
 
-    print(df)
+    df.to_csv('sp1500clean.csv')
 
 
 if True:
